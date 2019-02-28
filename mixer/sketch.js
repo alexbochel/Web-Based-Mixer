6cf687aebc4c716108746e180f2f39686eb666d3
omarjas <omarjas5@vt.edu>
2019-02-28 17:49:45 -0500

    track class

--- mixer/sketch.js
+++ mixer/sketch.js
@@ -1,36 +1,58 @@
-let vSlider;
 let play;
 
-let song;
+let tracks;
 
 function setup() {
   // put setup code here
   createCanvas(800, 800);
   noStroke();
 
-  //create slider
-  vSlider = createSlider(0, 10, 100);
-  vSlider.position(20,20);
+  tracks = [];
+  tracks.push(new Track('assets/sandman.mp3'));
+
   button = createButton('Play');
   button.position(170, 20);
-  song = loadSound('assets/sandman.mp3');
   button.mousePressed(togglePlay);
 }
 
 function draw() {
   // put drawing code here
-  song.amp(vSlider.value());
+  for (var i = 0; i < tracks.length; i++) {
+    if(tracks[i].file.isPlaying())
+      tracks[i].setVolume();
+  }
 }
 
 function togglePlay() {
-  if ( song.isPlaying() ) {
-    button.html("Play");
-    song.pause();
-  } else {
-    button.html("Pause");
-    song.play();
+  for (var i = 0; i < tracks.length; i++) {
+    if(tracks[i].file.isPlaying()) {
+      tracks[i].file.pause();
+      button.html("Play");
+    }
+    else {
+      tracks[i].file.play();
+      button.html("Pause");
+    }
   }
 }
 
 //FOR MUTE
 //SET AMP TO ZERO AND GET SLIDER VALUE ON UNMUTE
+
+class Track {
+  constructor(f) {
+    this.file = loadSound(f, this.loaded);
+    this.slider = createSlider(0,10,100);
+    this.slider.position(20,70);
+    this.slider.style('rotate', '-90');
+  }
+
+  setVolume() {
+    this.file.amp(this.slider.value());
+  }
+
+  loaded() {
+    console.log("loaded");
+  }
+
+}
